0d2acbb0609606f5a3dc04fd5ea0c830833cb105
Berenice Jacinto <berenice.jacinto.robledo@gmail.com>
2026-05-01 20:01:16 -0600

    Colocación y texturizado de los stands

--- ProyectoFinal/Proyecto_Final.cpp
+++ ProyectoFinal/Proyecto_Final.cpp
@@ -33,7 +33,7 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
 
 // Camera
-Camera  camera(glm::vec3(10.5f, 3.3f, 25.0f));
+Camera  camera(glm::vec3(-5.0f, 3.3f, 5.0f));
 GLfloat lastX = WIDTH / 2.0;
 GLfloat lastY = HEIGHT / 2.0;
 bool keys[1024];
