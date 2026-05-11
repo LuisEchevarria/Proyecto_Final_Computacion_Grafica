2537348eb1603cc4cdb097bf7c389cb848a15bc5
Berenice Jacinto <berenice.jacinto.robledo@gmail.com>
2026-05-10 19:24:07 -0600

    Acomodando la cámara

--- ProyectoFinal/Proyecto_Final.cpp
+++ ProyectoFinal/Proyecto_Final.cpp
@@ -163,7 +163,7 @@ const GLuint WIDTH = 800, HEIGHT = 600;
 int SCREEN_WIDTH, SCREEN_HEIGHT;
 
 // Camera
-Camera  camera(glm::vec3(-6.0f, 3.3f, -15.0f));
+Camera  camera(glm::vec3(-6.0f, 4.0f, -15.0f));
 GLfloat lastX = WIDTH / 2.0;
 GLfloat lastY = HEIGHT / 2.0;
 bool keys[1024];
